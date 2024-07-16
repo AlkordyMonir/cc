9a68890ce66b5963bb3962cbd5cc54f8d1848520
Monir ALKORDY <moneralkrd@gmail.com>
2024-07-16 14:46:27 +0200

    Add files via upload

---
+++ sec/test/java/Tikeo/TikeoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tikeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
